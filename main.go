00e793663abbb16727819f3919ddc3a1de730f7a
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-12 02:10:08 -0300

    feat: add TCP server

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	initialize_routes()
+
+	fmt.Println("Running on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Erro ao iniciar o servidor:", err)
+	}
+}
+
+func initialize_routes() {
+	routes := map[string]func(http.ResponseWriter, *http.Request){
+		"/hello": h1Handler,
+		"/":      notFoundHandler,
+	}
+
+	for path, handler := range routes {
+		http.HandleFunc(path, handler)
+	}
+}
+
+func h1Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h1>H1</h1>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "<h1>Page not found</h1>")
+}
