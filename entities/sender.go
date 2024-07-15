82ef58ef1175cae3221dd7c7d8dd7a194c0459c2
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-15 20:09:29 -0300

    feat: improve Sender methods to call Meta API

--- entities/sender.go
+++ entities/sender.go
@@ -1,14 +1,14 @@
 package entities
 
-type Buttons struct {
-	ButtonType string `json:"type"`
-	Title      string `json:"title"`
-	Payload    string `json:"payload"`
-}
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
 
 type Content struct {
-	Text    string    `json:"text"`
-	Buttons []Buttons `json:"buttons"`
+	Text    string   `json:"text"`
+	Buttons []Button `json:"buttons"`
 }
 
 type Sender struct {
@@ -17,13 +17,84 @@ type Sender struct {
 	Content     Content `json:"content"`
 }
 
-func (s *Sender) SendText(content string) {
-	s.MessageType = "text"
-	s.Content.Text = content
+type Recipient struct {
+	ID string `json:"id"`
 }
 
-func (s *Sender) SendButton(content string, buttons []Buttons) {
-	s.MessageType = "button"
-	s.Content.Text = content
-	s.Content.Buttons = buttons
+type MessageText struct {
+	Text string `json:"text"`
+}
+
+type RequestText struct {
+	Recipient Recipient   `json:"recipient"`
+	Message   MessageText `json:"message"`
+}
+
+type Button struct {
+	Type    string `json:"type"`
+	Title   string `json:"title"`
+	Payload string `json:"payload"`
+}
+
+type Payload struct {
+	TemplateType string   `json:"template_type"`
+	Text         string   `json:"text"`
+	Buttons      []Button `json:"buttons"`
+}
+
+type Attachment struct {
+	Type    string  `json:"type"`
+	Payload Payload `json:"payload"`
+}
+
+type MessageButton struct {
+	Attachment Attachment `json:"attachment"`
+}
+
+type RequestButton struct {
+	Recipient Recipient     `json:"recipient"`
+	Message   MessageButton `json:"message"`
+}
+
+func (s *Sender) SendText() (*http.Response, error) {
+	requestText := RequestText{
+		Recipient: Recipient{
+			ID: s.UserId,
+		},
+		Message: MessageText{
+			Text: s.Content.Text,
+		},
+	}
+	jsonRequestData, _ := json.Marshal(requestText)
+	response, err := http.Post("externalAPIURL", "application/json", bytes.NewBuffer(jsonRequestData))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	return response, nil
+}
+
+func (s *Sender) SendButton() (*http.Response, error) {
+	requestButton := RequestButton{
+		Recipient: Recipient{
+			ID: s.UserId,
+		},
+		Message: MessageButton{
+			Attachment: Attachment{
+				Type: "template",
+				Payload: Payload{
+					TemplateType: "button",
+					Text:         s.Content.Text,
+					Buttons:      s.Content.Buttons,
+				},
+			},
+		},
+	}
+	jsonData, _ := json.Marshal(requestButton)
+	response, err := http.Post("externalAPIURL", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	return response, nil
 }
