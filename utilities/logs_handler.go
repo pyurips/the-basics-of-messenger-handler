09910e92f09782a45ea7a72e805d58288da89f0e
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-15 17:39:49 -0300

    fix: change created log file name

--- utilities/logs_handler.go
+++ utilities/logs_handler.go
@@ -10,16 +10,19 @@ import (
 func CreateLogContent(logType string, userId string, pageId string, content string) {
 	fileName := ""
 	if pageId != "" {
-		fileName = fmt.Sprintf("logs/%s:%s.log", userId, pageId)
+		fileName = fmt.Sprintf("logs/%s_%s.log", userId, pageId)
 	} else {
-		fileName = fmt.Sprintf("logs/%s:nil.log", userId)
+		fileName = fmt.Sprintf("logs/%s.log", userId)
 	}
 
 	os.MkdirAll(filepath.Dir(fileName), 0755)
-	file, _ := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Error opening log file: %v", err)
+	}
 	defer file.Close()
 
-	logger := log.New(file, fmt.Sprintf("[%s]", logType), log.LUTC)
+	logger := log.New(file, fmt.Sprintf("[%s]", logType), log.Ldate|log.Ltime|log.LUTC)
 	logMessage := fmt.Sprintf("content: %s", content)
 	logger.Println(logMessage)
 }
