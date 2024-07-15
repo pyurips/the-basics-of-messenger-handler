33319c30afb7290e49afd29751024a19f4a17c54
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-15 15:42:34 -0300

    feat: add .env handler to validators and .env example file

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"the_basics_of_messenger_handler/configs"
+	"the_basics_of_messenger_handler/utilities"
 )
 
 func main() {
+	utilities.DotEnvHandler()
 	r := configs.InitializeRoutes()
 	r.Run(":8080")
 }
--- utilities/validators.go
+++ utilities/validators.go
@@ -2,8 +2,11 @@ package utilities
 
 import (
 	"encoding/json"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func JSONRequisitionParser(sender any, c *gin.Context) ([]byte, error, error) {
@@ -18,4 +21,22 @@ func JSONRequisitionParser(sender any, c *gin.Context) ([]byte, error, error) {
 	}
 
 	return jsonData, nil, nil
+}
+
+func DotEnvHandler() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env file not found")
+	}
+
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	emulator := os.Getenv("EMULATOR")
+
+	if accessToken == "" {
+		log.Fatal("ACCESS_TOKEN is not set in .env file")
+	}
+
+	if emulator == "" {
+		log.Fatal("EMULATOR is not set in .env file")
+	}
 }
