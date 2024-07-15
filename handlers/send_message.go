ccfc76ad78868cbf7cd9bbf467589624e39850a6
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-15 17:40:23 -0300

    feat: add log creator function to send endpoint

--- handlers/send_message.go
+++ handlers/send_message.go
@@ -15,10 +15,12 @@ func SendMessage(c *gin.Context) {
 	jsonData, bindError, marshalError := utilities.JSONRequisitionParser(&sender, c)
 	if bindError != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Error when binding JSON"})
+		utilities.CreateLogContent(entities.ERROR, "sender.UserId", "PAGE_ID", bindError.Error())
 		return
 	}
 	if marshalError != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error when marshalling JSON"})
+		utilities.CreateLogContent(entities.ERROR, sender.UserId, "PAGE_ID", marshalError.Error())
 		return
 	}
 
@@ -26,6 +28,7 @@ func SendMessage(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error when making a request to the external API"})
+		utilities.CreateLogContent(entities.ERROR, sender.UserId, "PAGE_ID", err.Error())
 		return
 	}
 	defer response.Body.Close()
