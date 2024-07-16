3e41e395d3640a9ddb8cf92a112daf6c7948e866
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-16 16:53:10 -0300

    feat: add receive message handler

--- handlers/receive_message.go
+++ handlers/receive_message.go
@@ -1,7 +1,18 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"the_basics_of_messenger_handler/entities"
+	"the_basics_of_messenger_handler/utilities"
+
+	"github.com/gin-gonic/gin"
+)
 
 func ReceiveMessage(c *gin.Context) {
-
+	receiver := entities.Receiver{}
+	utilities.JSONRequisitionParser(&receiver, c)
+	receiverJSON, _ := json.Marshal(receiver)
+	for _, messaging := range receiver.Messaging {
+		utilities.CreateLogContent("WEBHOOK", messaging.Sender.ID, string(receiverJSON))
+	}
 }
