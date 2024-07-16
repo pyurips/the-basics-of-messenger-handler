86bbb408c419912fe8b1428808d6a9a09d7c5a75
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-15 23:29:28 -0300

    fix: improve log prefix

--- handlers/send_message.go
+++ handlers/send_message.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"the_basics_of_messenger_handler/entities"
 	"the_basics_of_messenger_handler/utilities"
@@ -14,22 +15,27 @@ func SendMessage(c *gin.Context) {
 	bindError, marshalError := utilities.JSONRequisitionParser(&sender, c)
 	if bindError != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Error when binding JSON"})
-		utilities.CreateLogContent(entities.ERROR, "sender.UserId", "PAGE_ID", bindError.Error())
+		utilities.CreateLogContent(strconv.Itoa(http.StatusBadRequest), "sender.UserId", "PAGE_ID", bindError.Error())
 		return
 	}
 	if marshalError != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error when marshalling JSON"})
-		utilities.CreateLogContent(entities.ERROR, sender.UserId, "PAGE_ID", marshalError.Error())
+		utilities.CreateLogContent(strconv.Itoa(http.StatusInternalServerError), sender.UserId, "PAGE_ID", marshalError.Error())
 		return
 	}
 
-	utilities.MessageTypeCheck(&sender, c)
+	messageTypeError := utilities.MessageTypeCheck(&sender, c)
+	if messageTypeError != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid message type"})
+		utilities.CreateLogContent(strconv.Itoa(http.StatusBadRequest), sender.UserId, "PAGE_ID", messageTypeError.Error())
+		return
+	}
 
 	if sender.MessageType == "text" {
 		response, err := sender.SendText()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error when making a request to the external API"})
-			utilities.CreateLogContent(entities.ERROR, sender.UserId, "PAGE_ID", err.Error())
+			utilities.CreateLogContent(strconv.Itoa(http.StatusInternalServerError), sender.UserId, "PAGE_ID", err.Error())
 			return
 		}
 		c.JSON(response.StatusCode, nil)
@@ -39,7 +45,7 @@ func SendMessage(c *gin.Context) {
 		response, err := sender.SendButton()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error when making a request to the external API"})
-			utilities.CreateLogContent(entities.ERROR, sender.UserId, "PAGE_ID", err.Error())
+			utilities.CreateLogContent(strconv.Itoa(http.StatusInternalServerError), sender.UserId, "PAGE_ID", err.Error())
 			return
 		}
 		c.JSON(response.StatusCode, nil)
