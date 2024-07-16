371f38c4ee58e63c9f53261e2311edc5caa185b5
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-16 20:00:29 -0300

    fix: change local emulator endpoint

--- configs/endpoint_handler.go
+++ configs/endpoint_handler.go
@@ -6,7 +6,7 @@ func GetAPIEndpoint() string {
 	emulator := os.Getenv("EMULATOR")
 	accessToken := os.Getenv("ACCESS_TOKEN")
 	if emulator == "true" {
-		return "http://localhost:5000"
+		return "http://localhost:8081/?access_token=" + accessToken
 	}
 	return "https://graph.facebook.com/v2.6/me/messages?access_token=" + accessToken
 }
