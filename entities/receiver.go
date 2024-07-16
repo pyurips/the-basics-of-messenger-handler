7d8a2016ec11f1a3047ec2eecfed1d9b15008fc0
Pablo Yuri <pabloyuridev@gmail.com>
2024-07-16 16:16:37 -0300

    feat: add receiver entity

--- entities/receiver.go
+++ entities/receiver.go
@@ -1,1 +1,25 @@
 package entities
+
+type Messaging struct {
+	Sender    Recipient `json:"sender"`
+	Recipient Recipient `json:"recipient"`
+	Timestamp int64     `json:"timestamp"`
+	Message   *Message  `json:"message,omitempty"`
+	Postback  *Postback `json:"postback,omitempty"`
+}
+
+type Message struct {
+	Mid  string `json:"mid"`
+	Text string `json:"text"`
+}
+
+type Postback struct {
+	Mid     string `json:"mid"`
+	Payload string `json:"payload"`
+}
+
+type Receiver struct {
+	ID        string      `json:"id"`
+	Time      int64       `json:"time"`
+	Messaging []Messaging `json:"messaging"`
+}
